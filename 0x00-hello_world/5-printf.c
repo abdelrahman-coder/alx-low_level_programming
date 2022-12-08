735e24fa80e7b65856920383f284d29ebcf12e08
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2022-12-08 02:01:56 -0800

    q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-main - Entry point
-Return: Always 0 (Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 
@@ -14,3 +14,5 @@ int main(void)
 			return (0);
 
 }
+
+
