b17484036c0992a24d9f3da3996c12510611e72f
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2022-12-09 10:37:03 -0800

    q4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: 'Skipping Q and E'
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+		int ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+
+					{
+
+								if (ch == 'e' || ch == 'q')
+
+											{
+
+															continue;
+
+																	}
+
+										putchar(ch);
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
