cae49de095d9fca2fd2fa7356d17a81a0085dce2
abdelrahman-coder <63162992+abdelrahman-coder@users.noreply.github.com>
2022-12-09 20:45:34 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
-* main - Entry point
-* Description: 'Skipping Q and E'
-* Return: Always 0 (Success)
-*/
-/* betty style doc for function main goes there */
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
 int main(void)
 {
-	int ch;
+	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	while (ch <= 'z')
 	{
-		if (ch == 'e' || ch == 'q')
+		if (ch != 'e' && ch != 'q')
 		{
-			continue;
+			putchar(ch);
 		}
-		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
