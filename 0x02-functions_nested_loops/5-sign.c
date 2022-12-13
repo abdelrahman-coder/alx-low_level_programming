8a080925ed00108b9e58a9c649172c45de0fee16
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2022-12-13 12:53:13 -0800

    q

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,14 +3,15 @@
 /**
  * print_sign - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
- * Return: 1 if character is the number is greater than zer0, -1 for less than zer0.
+ * Return: 1 if character is the number is greater than zer0,
+ * -1 for less than zer0.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
