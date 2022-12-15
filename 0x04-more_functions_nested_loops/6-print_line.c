946d889db1c45c482836794bb12610de37283aca
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2022-12-15 10:57:46 -0800

    q6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,31 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - number fct
- * Bwave ICT file
- * Return: no return
+ * print_line - printing lines
+ * @n: number of times
+ * Return: void
  */
 
-void more_numbers(void)
+void print_line(int n)
 {
 
-	int i, j;
+	int i;
 
-		for (i = 1; i <= 10; i++)
+	for (i = 0; i < n; i++)
 
-		{
-			for (j = 0; j <= 14; j++)
+	{
+		_putchar('_');
 
-			{
+	}
 
-				if (j >= 10)
-				_putchar('1');
-				_putchar(j % 10 + '0');
-
-			}
-
-			_putchar('\n');
-
-			}
+		_putchar('\n');
 
 }
