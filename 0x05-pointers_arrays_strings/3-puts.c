03fb2e484b2ae44ab7e96ddbf75bb7b7549ec0e3
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2022-12-19 03:49:58 -0800

    q0

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prunts a string
+ * @str: the string
+ * return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		-putchar(str[i]);
+			i++;
+	}
+	_putchar('\n');
+}
