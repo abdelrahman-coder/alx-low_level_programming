c75ba991ef6a56fefc759b594dcbade043949ffd
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2023-01-06 06:49:47 -0800

    q0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,13 +15,14 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
-	printf("Error\n");
-	return(1);
+		printf("Error\n");
+		return(1);
 	}
+
 
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	printf("%d\n", x * y);
 
-	return (0)
+	return (0);
 }
