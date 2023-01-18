2f051eedb0b9e2369af3ea1dd4477d1a289e39a0
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2023-01-18 03:45:48 -0800

    q0

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,21 +1,11 @@
 #include "3-calc.h"
-
 #include <stdlib.h>
-
 #include <string.h>
 
-
-
 /**
-
-  * get_op_func - ...
-
+ * get_op_func - ...
   * @s: ...
-
-  *
-
   * Return: ...
-
   */
 
 int (*get_op_func(char *s))(int, int)
