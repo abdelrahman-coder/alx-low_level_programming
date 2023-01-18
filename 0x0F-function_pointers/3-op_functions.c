7ec8363a6bcf766922b95f833b9c481ea9943bb9
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2023-01-18 04:00:30 -0800

    q0

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,23 +1,12 @@
 #include "3-calc.h"
-
 #include <stdlib.h>
-
 #include <stdio.h>
 
-
-
 /**
-
   * op_add - ...
-
   * @a: ...
-
   * @b: ...
-
-  *
-
   * Return: ...
-
   */
 
 int op_add(int a, int b)
@@ -28,20 +17,11 @@ int op_add(int a, int b)
 
 }
 
-
-
 /**
-
   * op_sub - ...
-
   * @a: ...
-
   * @b: ...
-
-  *
-
   * Return: ...
-
   */
 
 int op_sub(int a, int b)
@@ -52,20 +32,11 @@ int op_sub(int a, int b)
 
 }
 
-
-
 /**
-
   * op_mul - ...
-
   * @a: ...
-
   * @b: ...
-
-  *
-
   * Return: ...
-
   */
 
 int op_mul(int a, int b)
@@ -75,21 +46,12 @@ int op_mul(int a, int b)
 	return (a * b);
 
 }
-
-
-
 /**
-
   * op_div - ...
-
   * @a: ...
-
   * @b: ...
-
   *
-
   * Return: ...
-
   */
 
 int op_div(int a, int b)
@@ -112,20 +74,11 @@ int op_div(int a, int b)
 
 }
 
-
-
 /**
-
   * op_mod - ...
-
   * @a: ...
-
   * @b: ...
-
-  *
-
   * Return: ...
-
   */
 
 int op_mod(int a, int b)
